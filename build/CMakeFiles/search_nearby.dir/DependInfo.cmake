6ce44e7f37cac614a6d3fd3657296f559139c84a
Anany Agrawal <138032335+anany-agrawal@users.noreply.github.com>
2025-12-30 14:38:30 +0000

    Make the project working

---
+++ build/CMakeFiles/search_nearby.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/search-nearby-2d-range-tree/src/PointDatabase.cpp" "CMakeFiles/search_nearby.dir/src/PointDatabase.cpp.o" "gcc" "CMakeFiles/search_nearby.dir/src/PointDatabase.cpp.o.d"
+  "/workspaces/search-nearby-2d-range-tree/src/RangeTree.cpp" "CMakeFiles/search_nearby.dir/src/RangeTree.cpp.o" "gcc" "CMakeFiles/search_nearby.dir/src/RangeTree.cpp.o.d"
+  "/workspaces/search-nearby-2d-range-tree/src/main.cpp" "CMakeFiles/search_nearby.dir/src/main.cpp.o" "gcc" "CMakeFiles/search_nearby.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/search_nearby.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_nearby.dir/src/PointDatabase.cpp.o"
+  "CMakeFiles/search_nearby.dir/src/PointDatabase.cpp.o.d"
+  "CMakeFiles/search_nearby.dir/src/RangeTree.cpp.o"
+  "CMakeFiles/search_nearby.dir/src/RangeTree.cpp.o.d"
+  "CMakeFiles/search_nearby.dir/src/main.cpp.o"
+  "CMakeFiles/search_nearby.dir/src/main.cpp.o.d"
+  "search_nearby"
+  "search_nearby.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_nearby.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
